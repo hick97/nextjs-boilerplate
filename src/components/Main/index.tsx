59490d76d6727f6c1d7dc3f4006cd3ecd560871f
Henrique Augusto <hick_97@hotmail.com>
2021-06-05 19:34:37 -0300

    feat: update Main component with styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,12 @@
-const Main = () => <main data-testid="main-section">The main Div</main>
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper data-testid="main__div--wrapper">
+    <S.Title data-testid="main__text--title">Lorem Ipsum</S.Title>
+    <S.Description data-testid="main__text--description">
+      Give me some description
+    </S.Description>
+  </S.Wrapper>
+)
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  align-items: center;
+  background-color: #06092b;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+`
+
+export const Logo = styled.img``
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
