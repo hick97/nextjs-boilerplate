b0341a54658b14f0e8fdf012a6778f8a4d32f015
Henrique Augusto <hick_97@hotmail.com>
2021-06-05 19:35:25 -0300

    test: update Main tests with styles improvement

--- src/components/Main/index.spec.tsx
+++ src/components/Main/index.spec.tsx
@@ -6,6 +6,17 @@ describe('<Main />', () => {
   it('should render the Main component', () => {
     const { getByTestId } = render(<Main />)
 
-    expect(getByTestId('main-section')).toBeInTheDocument()
+    expect(getByTestId('main__div--wrapper')).toBeInTheDocument()
+    expect(getByTestId('main__text--title')).toBeInTheDocument()
+    expect(getByTestId('main__text--description')).toBeInTheDocument()
+  })
+
+  it('should render the correct texts to Main title and description', () => {
+    const { getByTestId } = render(<Main />)
+
+    expect(getByTestId('main__text--title')).toHaveTextContent('Lorem Ipsum')
+    expect(getByTestId('main__text--description')).toHaveTextContent(
+      'Give me some description'
+    )
   })
 })
