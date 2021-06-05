52667af62750704baa116a0e6f804f6c54e2072e
Henrique Augusto <hick_97@hotmail.com>
2021-06-05 20:30:42 -0300

    feat: add first stories to Main component

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'Lorem Ipsum')}
+    description={text('Description', 'Give some description')}
+  />
+)
