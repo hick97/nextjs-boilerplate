7c9b7b2bfa08ce9c1265e80326704c1a78ef83be
Henrique Augusto <hick_97@hotmail.com>
2021-06-05 20:47:39 -0300

    fix: remove unecessary addons on storybook main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,6 @@ module.exports = {
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-actions",
-    "@storybook/addon-knobs/register"
   ]
 }
