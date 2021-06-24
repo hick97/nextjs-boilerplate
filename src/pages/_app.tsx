03795be90f8733379a240b68559e302669a89b86
Henrique Augusto <hick_97@hotmail.com>
2021-06-23 21:35:49 -0300

    feat: add initial configs to PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>My app</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel=" manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with Typescript, React, NextJS and Styled Component"
